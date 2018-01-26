06d702254be8cf073cc3867fe11ba98695a783c8
Ivan Kalita <kaduev13@gmail.com>
2018-01-26 22:42:37 +0300

    Add nonce and hash mining into the Block constructor.

--- impl/src/main/java/ru/kaduev/blockchain/impl/Block.java
+++ impl/src/main/java/ru/kaduev/blockchain/impl/Block.java
@@ -1,11 +1,7 @@
 package ru.kaduev.blockchain.impl;
 
-import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Block {
@@ -13,6 +9,7 @@ public class Block {
     private byte[] data;
     private byte[] prevBlockHash;
     private byte[] hash;
+    private long nonce;
 
     Block(String data, byte[] prevBlockHash) {
         this.timestamp = new Date();
@@ -23,33 +20,24 @@ public class Block {
         this.data = new byte[dataBytes.length];
         System.arraycopy(dataBytes, 0, this.data, 0, dataBytes.length);
 
-        this.setHash();
+        ProofOfWork.Proof proof = new ProofOfWork(this).run();
+        this.hash = proof.hash;
+        this.nonce = proof.nonce;
     }
 
     Block() {
         this("Genesis block", new byte[32]);
     }
 
-    private void setHash() {
-        ByteBuffer buffer = ByteBuffer.allocate(Long.SIZE + this.prevBlockHash.length + this.data.length);
-        buffer.put(this.prevBlockHash);
-        buffer.putLong(this.timestamp.getTime());
-        buffer.put(this.data);
-        try {
-            this.hash = MessageDigest.getInstance("SHA-256").digest(buffer.array());
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("No SHA-256 algorithm provider found.");
-        }
-
-
-    }
-
     public String toString() {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
-        return "Timestamp: " + df.format(this.timestamp.getTime()) + "\n" +
-                "P. Hash: " + HexHelper.printHexBinary(this.prevBlockHash) + "\n" +
-                "C. Hash: " + HexHelper.printHexBinary(this.hash) + "\n" +
-                "Data: " + new String(this.data) + "\n";
+        return String.format(
+                "Timestamp: %s\nP. Hash: %s\nC. Hash: %s\nData: %s\nNonce: %s\n",
+                df.format(this.timestamp.getTime()),
+                HexHelper.printHexBinary(this.prevBlockHash),
+                HexHelper.printHexBinary(this.hash),
+                new String(this.data),
+                nonce);
     }
 
     public Date getTimestamp() {
